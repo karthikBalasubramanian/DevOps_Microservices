0ce8dee6dfddc72586ecb1e17cbe26ba1f72191b
kabalasu <kabalasu@adobe.com>
2020-08-07 17:44:58 -0700

    Add kubernetes and docker related files

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,20 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=kabalasu/project-ml-microservice
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run kubernetes-project-ml-microservice\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=kuberneter-project-ml-microservice
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
+
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward kubernetes-project-ml-microservice 8000:80
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,12 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
-
-# Step 2:  
+dockerpath=kabalasu/project-ml-microservice
+docker tag project-ml-microservice $dockerpath
+# Step 2:
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
